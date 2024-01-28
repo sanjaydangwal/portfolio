e5cb433a0ef41d74f27ffaf133cca9108304c279
sanjay dangwal <sanjay.dangwal199@gmail.com>
2024-01-28 18:32:24 +0530

    Add App description, image, and links

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import pathlib
 
 st.set_page_config(layout="wide")
 
@@ -18,3 +20,21 @@ with col2:
     st.info(content)
 content2 = """Below you can find some of the apps I have built in Python. Feel free to contact me!"""
 st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.2, 1.5])
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df.iloc[0:len(df) // 2, ].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(str(pathlib.Path("images", row["image"])))
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df.iloc[len(df) // 2:, ].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(str(pathlib.Path("images", row["image"])))
+        st.write(f"[Source Code]({row['url']})")
